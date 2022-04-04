076a00a2bdea70db679b684d04133377488973fc
123archie <93096219+123archie@users.noreply.github.com>
2022-04-04 20:00:50 +0530

    improved front page

---
+++ script.js
@@ -0,0 +1,23 @@
+var btn1 = document.getElementById("nav-home-tab.nav-link");
+// var btn2 = document.getElementById("nav-profile-tab.nav-link");
+// var btn3 = document.getElementById("nav-contact-tab.nav-link");
+// var btn4 = document.getElementById("nav-contact1-tab.nav-link");
+// var btn5 = document.getElementById("nav-contact2-tab.nav-link");
+// var btn6 = document.getElementById("nav-contact3-tab.nav-link");
+ 
+// const array = [btn1, btn2, btn3, btn4, btn5, btn6];
+
+// flag = false;
+// for(let i =0 ; i<= 5; i++){
+//     array[i].onclick = function () {
+//         if (this.click == false) {
+//           this.setselected = false;
+//         }
+//       };
+// }
+
+
+// setInterval(function () {
+//   $(".active").removeClass("active"); //remove class active
+// }, 1000);
+
