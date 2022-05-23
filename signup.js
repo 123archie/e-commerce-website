bb15a608441aaf63f57e5f3493d3aae0838eb9c1
123archie <93096219+123archie@users.noreply.github.com>
2022-05-23 20:07:13 +0530

    changes

--- signup.js
+++ signup.js
@@ -1,10 +1,10 @@
-var name_user=document.getElementById("name")
-var button=document.getElementById("submit")
-var mailformat = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
-var email=document.getElementById("email")
-button.addEventListener('click', function performe(name_user,email,password){
-    //  if(email.value.match(mailformat))
-    //  alert("Valid Email")
-    //  document.
-    //  return true
-})
+function validateform(){
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
+    {
+        return(true)
+    }
+    else{
+        alert("Invalid Email")
+        return false
+    }
+ }
