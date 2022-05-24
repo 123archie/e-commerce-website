9e62cd7bec73e940efcb08f78fe2bc9bb6f871af
123archie <93096219+123archie@users.noreply.github.com>
2022-05-24 18:47:43 +0530

    Completed password toggle

--- signup.js
+++ signup.js
@@ -1,10 +1,32 @@
-function validateform(){
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
-    {
-        return(true)
-    }
-    else{
-        alert("Invalid Email")
-        return false
-    }
- }
+let button=document.getElementById('submit')
+button.addEventListener('click', function(){
+let email=document.getElementById('email');
+let validation = email.value;
+let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+if(regex.test(validation)){
+alert("valid email address");
+}
+else{
+alert("Invalid email address");
+}
+
+let password=document.getElementById('password');
+let validation_password=password.value;
+let regex_pass=/^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8}$/;
+if(regex_pass.test(validation_password)){
+    alert("Valid password");
+}else{
+    alert("Invalid password");
+}});
+
+let checkBOX=document.getElementById('checkbox')
+checkBOX.addEventListener('click', function(){
+let x=document.getElementById("password")
+if(x.type==='password'){
+x.type="text"
+}
+else{
+x.type='password'
+}
+});
+
